18cd8865897a7dfb1f6599c2b527057fb2c75e41
Nico Mayr <nico.mayr@tum.de>
2023-06-14 21:42:13 +0200

    init

---
+++ script.js
@@ -0,0 +1,34 @@
+// Create a SpeechRecognition object
+const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
+
+// Set parameters for the recognition
+recognition.lang = 'en-US';
+recognition.continuous = true;
+recognition.interimResults = true;
+
+recognition.onresult = (event) => {
+    let interimTranscript = '';
+    let finalTranscript = '';
+
+    for (let i = 0; i < event.results.length; ++i) {
+        const result = event.results[i];
+        const text = result[0].transcript;
+
+        if (result.isFinal) {
+            finalTranscript += text;
+        } else {
+            interimTranscript += text;
+        }
+    }
+
+    const transcriptionElement = document.getElementById('transcription');
+    //new interim and final transcriptions in two lines
+    transcriptionElement.innerHTML = `<div  class="interim">${interimTranscript}</div ><div  class="final">${finalTranscript}</div >`;
+};
+
+recognition.onend = () => {
+    // Restart the recognition when it ends
+    recognition.start();
+};
+
+recognition.start();
