09bc2b730870705b7521b588cb06b816334c4a75
Nico Mayr <nico.mayr@tum.de>
2023-06-16 00:24:15 +0200

    add cocktails

--- script.js
+++ script.js
@@ -14,15 +14,21 @@ recognition.onresult = (event) => {
         const result = event.results[i];
         const text = result[0].transcript;
 
+        let orderResult = null;
         if (result.isFinal) {
             finalTranscript += text;
+            orderResult = onTextChange(finalTranscript)
         } else {
             interimTranscript += text;
+            orderResult = onTextChange(interimTranscript)
+        }
+        if (orderResult !== null){
+            return orderResult;
         }
     }
 
     const transcriptionElement = document.getElementById('transcription');
-    //new interim and final transcriptions in two lines
+    //new interim text and final transcriptions in two lines
     transcriptionElement.innerHTML = `<div  class="interim">${interimTranscript}</div ><div  class="final">${finalTranscript}</div >`;
 };
 
@@ -32,3 +38,46 @@ recognition.onend = () => {
 };
 
 recognition.start();
+
+/////////////
+class Cocktail {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    isEqual(text) {
+        return text === this.id || text === this.name;
+    }
+}
+
+function onTextChange(entireText) {//detect cocktail
+    var trimmedText = entireText.trim();
+
+    const fs = require('fs');
+    const fileContents = fs.readFileSync('cocktails.csv', 'utf8');
+    const allCocktails = parseCSVFile(fileContents);
+
+    for (const cocktail in allCocktails){
+        if(cocktail.isEqual(trimmedText)){
+            return cocktail;
+        }
+    }
+    return null;
+}
+
+function parseCSVFile(csvData) {
+    const lines = csvData.split('\n');
+    const cocktails = [];
+
+    for (let i = 0; i < lines.length; i++) {
+        let line = lines[i].trim();
+        const [id, name] = line.split(',');
+        if (id && name) {
+            const cocktail = new Cocktail(id, name);
+            cocktails.push(cocktail);
+        }
+    }
+
+    return cocktails;
+}
