f0717718488b9bf9469aa3a4289ddd5bb67856e7
Nico Mayr <nico.mayr@tum.de>
2023-06-16 18:01:34 +0200

    integration

--- script.js
+++ script.js
@@ -1,3 +1,65 @@
+var csvData;
+var cocktailList;
+
+// Function to handle the CSV file response
+function handleCSVResponse(response) {
+  if (!response.ok) {
+    console.error('Failed to load the CSV file:', response.status, response.statusText);
+    return;
+  }
+
+  response.text().then(function (contents) {
+    csvData = contents; // Store the CSV data in the global variable
+    console.log('CSV data:', csvData);
+    cocktailList = parseCSVFile(csvData)
+    console.log(cocktailList)
+
+    // Get the container element where the table will be displayed
+    var container = document.getElementById('order-list'); 
+    // Generate the HTML table and insert it into the container
+    container.innerHTML = generateHTMLTable(contents);
+});
+}
+
+// Fetch the CSV file from the server
+fetch('cocktails.csv')
+  .then(handleCSVResponse)
+  .catch(function (error) {
+    console.error('Failed to fetch the CSV file:', error);
+  });
+
+
+function generateHTMLTable(csvData) {
+    var lines = csvData.split('\n'); // Split CSV into lines
+    var tableHTML = '<table>';
+  
+    // Generate table header
+    var headers = lines[0].split(',');
+    tableHTML += '<tr>';
+    for (var i = 0; i < headers.length; i++) {
+      tableHTML += '<th>' + headers[i] + '</th>';
+    }
+    tableHTML += '</tr>';
+  
+    // Generate table rows
+    for (var j = 1; j < lines.length; j++) {
+      var row = lines[j].split(',');
+      tableHTML += '<tr>';
+      for (var k = 0; k < row.length; k++) {
+        tableHTML += '<td>' + row[k] + '</td>';
+      }
+      tableHTML += '</tr>';
+    }
+  
+    tableHTML += '</table>';
+  
+    return tableHTML;
+  }
+
+
+
+
+///////////////
 // Create a SpeechRecognition object
 const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
 
@@ -17,10 +79,10 @@ recognition.onresult = (event) => {
         let orderResult = null;
         if (result.isFinal) {
             finalTranscript += text;
-            orderResult = onTextChange(finalTranscript)
+            //orderResult = onTextChange(finalTranscript)
         } else {
             interimTranscript += text;
-            orderResult = onTextChange(interimTranscript)
+            //orderResult = onTextChange(interimTranscript)
         }
         if (orderResult !== null){
             return orderResult;
@@ -54,9 +116,7 @@ class Cocktail {
 function onTextChange(entireText) {//detect cocktail
     var trimmedText = entireText.trim();
 
-    const fs = require('fs');
-    const fileContents = fs.readFileSync('cocktails.csv', 'utf8');
-    const allCocktails = parseCSVFile(fileContents);
+
 
     for (const cocktail in allCocktails){
         if(cocktail.isEqual(trimmedText)){
@@ -70,7 +130,7 @@ function parseCSVFile(csvData) {
     const lines = csvData.split('\n');
     const cocktails = [];
 
-    for (let i = 0; i < lines.length; i++) {
+    for (let i = 1; i < lines.length; i++) {//ignore header
         let line = lines[i].trim();
         const [id, name] = line.split(',');
         if (id && name) {
